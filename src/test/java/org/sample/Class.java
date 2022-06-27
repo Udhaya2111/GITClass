4a2a3345539a0762fcac954f420322f19f46f1da
Udhaya2111 <cr7udhayakumar@gmail.com>
2022-06-27 17:25:36 +0530

    push method

--- src/test/java/org/sample/Class.java
+++ src/test/java/org/sample/Class.java
@@ -5,4 +5,13 @@ private void method1() {
 		System.out.println("1");
 
 	}
+private void method4() {
+	System.out.println("kannaiki");
+
+}
+
+private void method5() {
+	System.out.println("siva");
+
+}
 }
